3a349ad7a94b92a0549eb6e96a320eb7b8d7f63a
root <root@purple-owl-21110.zap.cloud>
2026-06-10 16:57:47 +0000

    added scroll to table

--- html/website1/src/main.ts
+++ html/website1/src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers ?? []),
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {
+        subscriptSizing: 'dynamic'
+      }
+    }
+  ]
+}).catch(err => console.error(err));
+
+//bootstrapApplication(App, appConfig)
+//  .catch((err) => console.error(err));
